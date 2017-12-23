46d8cdf9f1fd7c0cc8249de448cda729845fb5ba
Sjoerd de Vries <sjdv1982@gmail.com>
2017-12-23 02:00:43 +0100

    fixed widgetlink in multiple browsers

--- seamless/gui/widgetlink.py
+++ seamless/gui/widgetlink.py
@@ -2,13 +2,20 @@ import traitlets
 from collections import namedtuple
 import traceback
 
-def widgetlink(c, w, as_data=False):
+def widgetlink(c, w, as_data=False, multi_browser=True):
     from ..core import Cell
     from .. import observer
     assert isinstance(c, Cell)
     assert isinstance(w, traitlets.HasTraits)
     assert w.has_trait("value")
     handler = lambda d: c.set(d["new"])
+    if multi_browser:
+        # If there are multiple browsers for the same notebook, with the same
+        #  kernel, we need to propagate the value back to the browser(s)
+        # https://github.com/jupyter-widgets/ipywidgets/issues/1881
+        def handler(d):
+            d["owner"].send_state("value")
+            c.set(d["new"])
     value = c.data if as_data else c.value
     if value is not None:
         w.value = value
